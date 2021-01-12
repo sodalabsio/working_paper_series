907c6ad7a0a4025dcb5d1aa5a6487bfd846d9d60
syborg91 <satyabratborgohain@gmail.com>
2021-01-12 12:46:41 +1100

    added Update functionality and fixed caching issue

--- sodalabs.io/assets/js/wps.js
+++ sodalabs.io/assets/js/wps.js
@@ -1,41 +1,100 @@
 $(document).ready(function() {
+  // old method --
+    // const formatNumber = n => ("0" + n).slice(-2);
+    // const base_url = "http://sodalabs.io.s3-ap-southeast-2.amazonaws.com/metadata.json";
+    // let wpn = ""
+    // const date = new Date();
+    // const maxAllowedSize = 5 * 1024 * 1024; // 5 MB
+    // $.ajax({
+    //   type: "GET",
+    //   // XDomainRequest protocol must be the same scheme as the calling page
+    //   url: base_url, // ('https:' == document.location.protocol ? 'https://' : 'http://') +
+    //   dataType: "json",
+    //   success: function(data) {
+    //     // console.log(data)
+    //     // check if JSON file is stringified
+    //     if (typeof data == 'string'){
+    //       data = JSON.parse(data)
+    //     }
+    //     // console.log(data)
+    //     currentYear = date.getFullYear();
+    //     if (data.papers.length > 0){
+    //       let currentPapers = data.papers.filter(a=>a.year==currentYear);
+    //       wpn = currentYear + "-" + formatNumber(currentPapers.length+1) // note: only handles 01-99
+    //     } 
+    //     else{
+    //       wpn = currentYear + "-" + "01"
+    //     }
+    //     $("#wpn").attr("placeholder", wpn); // set the placeholder
+    //     $("#wpn").val(wpn)
+    //   },
+    //   error: function(error) {
+    //     console.log(`Error ${error}`)
+    //     $("#errorModal .modal-body").html("");
+    //     $('#errorModal .modal-body').prepend(`<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`)
+    //     $("#spinner").remove();
+    //     $('button').prop('disabled', false);
+    //     $('#errorModal').modal('show');
+    //   }
+    //   });
+
+    function triggerError(msg){
+      $("#errorModal .modal-body").html("");
+      $('#errorModal .modal-body').prepend(msg)
+      $("#spinner").remove();
+      $('button').prop('disabled', false);
+      $('#errorModal').modal('show');
+    }
+
     const formatNumber = n => ("0" + n).slice(-2);
-    const base_url = "http://sodalabs.io.s3-ap-southeast-2.amazonaws.com/metadata.json";
     let wpn = ""
     const date = new Date();
     const maxAllowedSize = 5 * 1024 * 1024; // 5 MB
-    $.ajax({
-      type: "GET",
-      // XDomainRequest protocol must be the same scheme as the calling page
-      url: base_url, // ('https:' == document.location.protocol ? 'https://' : 'http://') +
-      dataType: "json",
-      success: function(data) {
-        // console.log(data)
-        // check if JSON file is stringified
-        if (typeof data == 'string'){
-          data = JSON.parse(data)
+    let currentYear = date.getFullYear(); 
+    let currentPapers = 0;
+    let prefix = `RePEc/ajr/sodwps/${currentYear}-`;
+    // AWS credentials
+    let bucketName = "soda-wps";
+    const bucketRegion = "ap-southeast-2";
+    const IdentityPoolId = "ap-southeast-2:39b53048-8af5-475b-9c7e-24057d7f4b71";
+    
+    // aws cognito
+    AWS.config.update({
+      region: bucketRegion,
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+      })
+    });
+    let s3 = new AWS.S3({
+      // apiVersion: "2012-10-17",
+      params: { Bucket: bucketName }
+    });
+    var params = {
+      Bucket: bucketName,
+      Delimiter: '/',
+      Prefix: prefix
+     }
+     // get the no of papers
+     // Test - max key limit (1000?)
+     s3.listObjects(params, function (error, data) {
+      if (error){
+        console.log(`Error ${error}`)
+        let msg = `<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`
+        triggerError(msg)
+      }
+      else{
+        currentPapers = data.Contents.length / 2 // ignoring RDF files
+        if (currentPapers > 0){ // if there are papers
+          wpn = currentYear + "-" + formatNumber(currentPapers+1) // note: only handles 01-99
         }
-        // console.log(data)
-        currentYear = date.getFullYear();
-        if (data.papers.length > 0){
-          let currentPapers = data.papers.filter(a=>a.year==currentYear);
-          wpn = currentYear + "-" + formatNumber(currentPapers.length+1) // note: only handles 01-99
-        } 
         else{
           wpn = currentYear + "-" + "01"
         }
         $("#wpn").attr("placeholder", wpn); // set the placeholder
         $("#wpn").val(wpn)
-      },
-      error: function(error) {
-        console.log(`Error ${error}`)
-        $("#errorModal .modal-body").html("");
-        $('#errorModal .modal-body').prepend(`<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`)
-        $("#spinner").remove();
-        $('button').prop('disabled', false);
-        $('#errorModal').modal('show');
       }
-      });
+     });
+
       // Add the following code if you want the name of the file appear on select
       $(".custom-file-input").on("change", function() {
         let fileName = $(this).val().split("\\").pop();
@@ -85,19 +144,78 @@ $(document).ready(function() {
 
           fileSize = $('#inputFile')[0].files[0].size
           if (fileSize > maxAllowedSize){
-            $("#errorModal .modal-body").html("");
-            $('#errorModal .modal-body').prepend(`<p><strong>File too large.</strong></p><p>Please upload a PDF file which is less than 5 MB in size.</p>`)
-            $("#spinner").remove();
-            $('button').prop('disabled', false);
-            // $(document).scrollTop($(document).height()); 
-            // $('#msg').html(msg);
-            $('#errorModal').modal('show');
+            let msg = `<p><strong>File too large.</strong></p><p>Please upload a PDF file which is less than 5 MB in size.</p>`
+            triggerError(msg)
           }
           else{
             $('#confirmModal').modal('show');
           }
         }
     });
+
+    $("#confirmUpdate").click(function(e) {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+     var form = document.getElementById('wpFormUpdate');
+     if (form.checkValidity() === false) {
+       event.preventDefault();
+       event.stopPropagation();
+       form.classList.add('was-validated');
+     }
+     else{
+
+       fileSize = $('#inputUpdateFile')[0].files[0].size
+       if (fileSize > maxAllowedSize){
+         let msg = `<p><strong>File too large.</strong></p><p>Please upload a PDF file which is less than 5 MB in size.</p>`
+         triggerError(msg)
+       }
+       else{
+        wpn = $('#wpn-update').val()
+        bucketName = "sodalabs.io"
+        let s3 = new AWS.S3({
+          // apiVersion: "2012-10-17",
+          params: { Bucket: bucketName }
+        });
+        var params = {
+          Bucket: bucketName, 
+          Key: "metadata.json"
+         };
+        // fetch details from metadata.json
+        s3.getObject(params, function (error, data) {
+          if (error){
+            console.log(`Error ${error}`)
+            let msg = `<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`
+            triggerError(msg)
+          }
+          else{
+            data = data.Body.toString('utf-8'); // Use the encoding necessary
+            if (typeof data == 'string'){
+              data = JSON.parse(data)
+            }
+            console.log(data)
+            let currentPaper = data.papers.filter(a=>a.wpn==wpn);
+            console.log(currentPaper)
+            if (currentPaper.length > 0){
+              $("#updateModal .modal-body").html("");
+              let verifyMsg = `<p>You are about to overwrite the following paper: 
+              <ul>
+                <li>WPN: <b>${currentPaper[0].wpn}</b></li>
+                <li>Title: <b>${currentPaper[0].title}</b></li>
+                <li>Authors: <b>${currentPaper[0].author}</b></li>
+              </ul> with a new version.</p>
+                <p>Doing so will make the existing version unavailable and only the new version will be available from now.</p>
+                <p>If you wish to continue, write “PROCEED” and in all-caps below and click ENTER, or click Cancel, to go back</p>`
+              $('#updateModal .modal-body').prepend(verifyMsg)
+              $('#updateModal').modal('show');
+            }
+            else{
+              let msg = `<p><strong>Oops!</strong></p><p>The paper you requested for does not exist. Please enter a valid working paper number.</p>`
+              triggerError(msg)
+            }
+          }
+         });
+       }
+     }
+ });
 
     $("#closeBtn").click(function(e) {
       $('form').get(0).reset();
@@ -112,6 +230,7 @@ $(document).ready(function() {
         
     //   };
     // });
+
 
       $("#submitPaper").click(function(e) {
         // e.preventDefault();
@@ -151,7 +270,8 @@ $(document).ready(function() {
                         jel_code:  $('#jel').val(),
                         abstract: encodeURIComponent($('#abstract').val()),
                         pub_online:  date.getDate() + ' ' + date.toLocaleString('default', { month: 'long' }) + ' ' + date.getFullYear(),
-                        file: base64
+                        file: base64,
+                        mode: 'upload'
                     }
                 // console.log(data)
                 // $('#messageModal').on('hide', function() {
@@ -170,20 +290,20 @@ $(document).ready(function() {
                     data: data,
                     success: function (response) {
                       console.log(response)      
-                      //   msg = `<div class="alert alert-success alert-dismissible fade show" role="alert">
-                      //   <strong>Done!</strong> Your paper has been successfully submitted. Here's the link below: <a href="${response.body.url}">${response.body.url}</a>
-                      //   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                      //     <span aria-hidden="true">&times;</span>
-                      //   </button>
-                      // </div>`
-                      $("#messageModal .modal-body").html("");
-                      $('#messageModal .modal-body').prepend(`<p><strong>Done!</strong></p><p>Your paper has been successfully submitted. Here's the link below:</p><p><a href="${response.body.url}">${response.body.url}</a></p>`)
-                        $("#spinner").remove();
-                        $('button').prop('disabled', false);
-                        // $(document).scrollTop($(document).height()); 
-                        // $('#msg').html(msg);
-                        $('#messageModal').modal('show');
-                        console.log('Done!')
+                      if ('errorMessage' in response){
+                        let msg = `<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`
+                        triggerError(msg)
+                      }
+                      else {
+                        $("#messageModal .modal-body").html("");
+                        $('#messageModal .modal-body').prepend(`<p><strong>Done!</strong></p><p>Your paper has been successfully submitted. Here's the link below:</p><p><a href="${response.body.url}">${response.body.url}</a></p>`)
+                          $("#spinner").remove();
+                          $('button').prop('disabled', false);
+                          // $(document).scrollTop($(document).height()); 
+                          // $('#msg').html(msg);
+                          $('#messageModal').modal('show');
+                          console.log('Done!')
+                      }
                     },
                     error: function(){
                         console.log("error!") 
@@ -194,17 +314,68 @@ $(document).ready(function() {
                       //   </button>
                       // </div>`
                       //   $('#msg').html(msg);
-                        $("#errorModal .modal-body").html("");
-                        $('#errorModal .modal-body').prepend(`<p><strong>Oops!</strong></p><p>There's been an error.</p>`)
-                        $("#spinner").remove();
-                        $('button').prop('disabled', false);
-                        // $(document).scrollTop($(document).height()); 
-                        // $('#msg').html(msg);
-                        $('#errorModal').modal('show');
+                      let msg = `<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`
+                      triggerError(msg)
                     }
                 });
                   };
                   reader.readAsDataURL(file.files[0]);
             // }
           });
+
+
+          $("#updatePaper").click(function(e) {
+            
+              $('#updateModal').modal('hide');
+              $('button').prop('disabled', true);
+              $('#confirmUpdate').prepend(`<span id="spinner" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`);
+              console.log('Processing ..')
+
+
+                      let base64;
+                      let author = [];
+                      // var form = document.getElementById('wpForm');
+                      var reader = new FileReader(),
+                      file = $('#inputUpdateFile')[0];
+                      reader.onload = function () {
+                          let result = reader.result;
+                          base64 = result.replace(/^[^,]*,/, '')
+                          // all values are string
+                          let data = {
+                            wpn : $('#wpn-update').val(),
+                            file: base64,
+                            mode: 'update'
+                        }
+                    $.ajax({
+                        url: "https://5v0dil8zg2.execute-api.ap-southeast-2.amazonaws.com/v1/upload",
+                        type: "POST",
+                        contentType: 'application/json',
+                        dataType: 'json',
+                        accept: 'application/json',
+                        processData: true,
+                        data: data,
+                        success: function (response) {
+                            console.log(response)      
+                            if ('errorMessage' in response){
+                              let msg = `<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`
+                              triggerError(msg)
+                            }
+                            else{
+                              $("#messageModal .modal-body").html("");
+                              $('#messageModal .modal-body').prepend(`<p><strong>Done!</strong></p><p>Your paper has been successfully updated. Here's the link below:</p><p><a href="${response.body.url}">${response.body.url}</a></p>`)
+                              $("#spinner").remove();
+                              $('button').prop('disabled', false);
+                              $('#messageModal').modal('show');
+                              console.log('Done!')
+                            }
+                        },
+                        error: function(){
+                          let msg = `<p><strong>Oops!</strong></p><p>An error has occurred. Please try again later.</p>`
+                          triggerError(msg)
+                        }
+                    });
+                      };
+                      reader.readAsDataURL(file.files[0]);
+                // }
+              });
   });
